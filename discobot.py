0b732e60353d473b30433310ebdcb0de8541181f
este001 <estefan.rm2@gmail.com>
2020-03-15 19:33:12 +0100

    initial commit

--- discobot.py
+++ discobot.py
@@ -1,37 +1,19 @@
 import os
 import discord
+import time
 from dotenv import load_dotenv
 
 
-def doit(*args):
+def doit():
     load_dotenv()
     TOKEN = os.getenv('DISCORD_TOKEN')
-    GUILD = os.getenv('DISCORD_GUILD')
-
     client = discord.Client()
 
     @client.event
     async def on_ready():
-        # for guild in client.guilds:
-        #     if guild.name == GUILD:
-        #         break
-        # print(
-        #     f'{client.user} is connected to the following guild:\n'
-        #     f'{guild.name}(id: {guild.id})\n'
-        #     f'{client.guilds[0]} hallå'
-        # )
-
-        if not all(args):
-            channel = client.get_channel(688709593584631831)
-            await channel.send('Grades are set')
-        else:
-            channel = client.get_channel(688709593584631831)
-            await channel.send(args[0])
+        print("test")
+        channel = client.get_channel(688709593584631831)
+        await channel.send('Grades are set')
+        os._exit(0)
 
     client.run(TOKEN)
-
-if __name__ == '__main__':
-    doit()
-
-
-
--- scrape.py
+++ scrape.py
@@ -1,11 +1,11 @@
 import time
+import asyncio
 from discobot import doit
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-
 if __name__ == '__main__':
-    driver = webdriver.Chrome(r"C:\Users\estef\Downloads\driver\chromedriver.exe")
+    driver = webdriver.Chrome(r"/usr/lib/chromium-browser/chromedriver")
     try:
 
         driver.get('https://nackademin.learnpoint.se/')
@@ -13,28 +13,37 @@ if __name__ == '__main__':
         loginUser = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtUserName')
         loginUser.send_keys('syh15s010')
         loginPass = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtPassword')
-        loginPass.send_keys('1Atkk5a7d1210')
+        loginPass.send_keys('Atkk5a7d1210')
         loginButton = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$btnLogin')
         loginButton.click()
 
         studiePlan = driver.find_element_by_link_text('Studieplan')
         studiePlan.click()
 
-        #At this moment the tag where windows server grade has the id ..ct105_lblMarkName
-        course = 5
-        while(True):
+        # At this moment the tag where windows server grade has the id ..ct105_lblMarkName
+        course = 4
+        while True:
+
+            print(course)
             windowsServerBetyg = driver.find_element_by_id(
                 f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl0{course}_lblMarkName')
-            if(windowsServerBetyg==''):
+            if windowsServerBetyg.text == '':
                 time.sleep(3600)
                 continue
             else:
+                time.sleep(1)
+                driver.get(driver.current_url)
+                #course += 1
                 doit()
+                print("test4")
+
+            print("test5")
+
+
     except NoSuchElementException as e:
         driver.close()
+        print(e)
         err = str(e)
         file1 = open("error.txt", "a")  # append mode
         file1.write(err)
         file1.close()
-        doit(e)
-
