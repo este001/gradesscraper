436badc961f82cd293e6f09baa213fc4c68e8f40
este001 <estefan.rm2@gmail.com>
2020-07-02 16:22:48 +0200

    refactored v2

--- scrape.py
+++ scrape.py
@@ -13,16 +13,29 @@ Save current id in txtfile?
 """
 
 
+def announce_grade(driver):
+    with open("grade_id", "r") as file:
+        id = file.readline()
+        if driver.find_element_by_id\
+            (f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl{int(id):02d}_lblMarkName').text !="":
+            find_ungraded_course(driver)
+            driver.close()
+            doit()
+        else:
+            driver.close()
 
-def course_grade():
-
-
-    pass
-
-
+def find_ungraded_course(driver):
+    #id14 is last course
+    for id in range(15):
+        if driver.find_element_by_id(f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl{id:02d}_lblMarkName').text =="":
+            with open("grade_id", "w") as file:
+                file.write(str(id))
+                file.close
+            break
+        
 def login_user(driver):
     #Enters credentials
-    print(driver)
+    driver.get('https://nackademin.learnpoint.se/')
     loginUser = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtUserName')
     loginUser.send_keys('syh15s010')
     loginPass = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtPassword')
@@ -42,12 +55,14 @@ def click_studieplan(driver):
 def scrape():
     #Surfs to studentportalen
     driver = webdriver.Chrome()
-    return driver.get('https://nackademin.learnpoint.se/')
+    return driver
 
 if __name__ == '__main__':
     driver = scrape()
     login_user(driver)
     click_login(driver)
     click_studieplan(driver)
+    announce_grade(driver)
+
     #driver = webdriver.Chrome(r'C:\Users\estef\Downloads\driver\chromedriver.exe')
-    #windowsServerBetyg = driver.find_element_by_id(f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl0{course}_lblMarkName')
+   
