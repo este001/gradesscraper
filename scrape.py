957c0ccad05e8639af99878bc80a07e11f2a971c
este001 <estefan.rm2@gmail.com>
2020-07-02 14:52:06 +0200

    refactoring

--- scrape.py
+++ scrape.py
@@ -4,43 +4,50 @@ from discobot import doit
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-if __name__ == '__main__':
+
+"""
+It has to find course without grade, and announce latest course grade.
+Then save current empty course grade
+If course grade is != empty, repeat.
+Save current id in txtfile?
+"""
+
+
+
+def course_grade():
+
+
+    pass
+
+
+def login_user(driver):
+    #Enters credentials
+    print(driver)
+    loginUser = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtUserName')
+    loginUser.send_keys('syh15s010')
+    loginPass = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtPassword')
+    loginPass.send_keys('Atkk5a7d1210')
+
+def click_login(driver):
+    #Clicks on login
+    loginButton = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$btnLogin')
+    loginButton.click()
+
+
+def click_studieplan(driver):
+    #Clicks on hamburgermenu Studieplan
+    studiePlan = driver.find_element_by_link_text('Studieplan')
+    studiePlan.click()
+
+def scrape():
+    #Surfs to studentportalen
     driver = webdriver.Chrome()
+    return driver.get('https://nackademin.learnpoint.se/')
+
+if __name__ == '__main__':
+    driver = scrape()
+    login_user(driver)
+    click_login(driver)
+    click_studieplan(driver)
     #driver = webdriver.Chrome(r'C:\Users\estef\Downloads\driver\chromedriver.exe')
-    try:
-
-        driver.get('https://nackademin.learnpoint.se/')
-
-        loginUser = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtUserName')
-        loginUser.send_keys('syh15s010')
-        loginPass = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$txtPassword')
-        loginPass.send_keys('Atkk5a7d1210')
-        loginButton = driver.find_element_by_name('ctl00$MainContentPlaceHolder$ctlUserLoginControl$btnLogin')
-        loginButton.click()
-
-        studiePlan = driver.find_element_by_link_text('Studieplan')
-        studiePlan.click()
-
-        # At this moment the tag where windows server grade has the id ..ct105_lblMarkName
-        course = 5
-        while True:
-            windowsServerBetyg = driver.find_element_by_id(
-                f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl0{course}_lblMarkName')
-            if windowsServerBetyg.text == '':
-                driver.close()
-                os._exit(0)       
-            else:
-                time.sleep(1)
-                driver.get(driver.current_url)
-                driver.close()
-                #course += 1
-                doit()
-
-
-    except NoSuchElementException as e:
-        driver.close()
-        print(e)
-        err = str(e)
-        file1 = open("error.txt", "a")  # append mode
-        file1.write(err)
-        file1.close()
+    #windowsServerBetyg = driver.find_element_by_id(f'ctl00_MainContentPlaceHolder_ctlCoursesOfStudentControl_rptCourses_ctl0{course}_lblMarkName')
