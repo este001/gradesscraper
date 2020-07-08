8d316c094b0bba9198399d603436e62f6763a75a
este001 <estefan.rm2@gmail.com>
2020-07-08 20:23:34 +0200

    added some prints and new lines

--- test_scrape.py
+++ test_scrape.py
@@ -1,28 +1,34 @@
 import unittest
 import scrape
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 class TestScrape(unittest.TestCase):
 
     def setUp(self):
-        self.driver = scrape.scrape()
+        
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.get('https://nackademin.learnpoint.se/')
-        print("Browser Opened\n")
+        print("\nBrowser Opened")
 
     def test_driver(self):
         self.assertTrue(self.driver)
-        print("driver found...\n")
+        print("\ndriver found...")
 
     def test_open_learpoint(self):
         expected_url = 'https://nackademin.learnpoint.se/LoginForms/LoginForm.aspx?ReturnUrl=%2f'
         expected_title = 'Nackademin - LearnPoint'
         self.assertEqual(expected_url, scrape.get_url(self.driver).current_url )
         self.assertEqual(expected_title, scrape.get_url(self.driver).title)
-        print("checking if site is reachable...\n")
+        print("\nchecking if site is reachable...")
        
     def test_login_learnpoint(self):
         scrape.login_user(self.driver)
-        print("Trying to login with credentials...\n")
+        print("\nTrying to login with credentials...")
 
 
     def tearDown(self):
